6a6969057f782545f78009acaf2da993cbbd8a7b
Jotappe <jotappe-@hotmail.com>
2022-01-25 14:56:34 +0000

    css custom

--- pages/index.js
+++ pages/index.js
@@ -83,7 +83,7 @@ export default function PaginaInicial() {
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas!</Titulo>
+            <Titulo tag="h2">Bem vindo piloto 6.66!</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
